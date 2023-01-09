3900a6d6e2cf7e074517a58a462dffdba2b7e1f5
Vinicius Figueiredo <viniciusfifer@gmail.com>
2023-01-09 15:42:14 -0300

    make connection with index.html

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 import express from "express";
 import port from "./config.js";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+
 const app = express();
 
-app.set('port', port || 3000);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
-  });
+  res.send('Hello World!');
+});
+
+app.set('port', port || 3000);
 
 app.listen(app.get('port'));
